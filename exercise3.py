6fedf9589c365463e2ea01509ac36e3482d89d90
Girish M <girishm@mailfence.com>
2020-04-24 23:12:52 +0530

    1.less than a number 2.divisors of a number

---
+++ exercise3.py
@@ -0,0 +1,4 @@
+n = int(input("enter a number"))
+for num in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]:
+    if n > num:
+        print(num)
---
+++ exercise4.py
@@ -0,0 +1,3 @@
+f = int(input("enter a number"))
+print([n for n in range(1,f+1) if f%n == 0])
+
