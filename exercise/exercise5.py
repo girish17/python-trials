c267b7b0458ea68d60539e16749d4e58258da6d8
Girish M <girishm@mailfence.com>
2020-04-28 23:51:14 +0530

    list comprehensions

--- exercise/exercise5.py
+++ exercise/exercise5.py
@@ -2,7 +2,5 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = []
-for num1 in a:
-    if num1 in b and num1 not in c:
-        c.append(num1)
-print(c)
+print(list(set([num1 for num1 in a if num1 in b and num1 not in c])))
+
---
+++ exercise/exercise6.py
@@ -0,0 +1,2 @@
+# @author: prince
+print([n for n in [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] if n%2 == 0])
