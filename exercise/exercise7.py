2dcff8f0990c4fc48ce05a842aea41d64787eb7d
Elijah999 <elijahkr2018@gmail.com>
2020-05-05 13:09:11 -0400

    added the factor that's divisible by your number

--- exercise/exercise7.py
+++ exercise/exercise7.py
@@ -2,13 +2,14 @@ import math
 n = int(input('enter a number: '))
 prime = True
 if n%2 == 0 and n != 2:
-    print('composite')
+    print('composite because it is even and not two')
     prime = False
 if n%2 != 0:
     sq = int(math.sqrt(n))
     for factor in range(3, sq):
         if n%factor == 0:
-            print('composite')
+            
+            print('composite, because', factor, 'is a factor of', n)
             prime = False
             break
        
