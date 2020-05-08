5516092d294287c73ffb737e029ddd314adcce4b
Elijah999 <elijahkr2018@gmail.com>
2020-05-08 13:34:42 -0400

    password generator and list ends

---
+++ exercise/exercise9.py
@@ -0,0 +1,2 @@
+import random
+print(random.choice(["Throwing", "Catching", "Active", "Eating", "Coding", "Sitting", "Watching", "Destroying", "Boarding", "Gaming"])+random.choice(["Chair", "Teammate", "Brownie", "Closet", "Box", "Blanket", "Carpet", "Pillow", "Rug", "Console"])+str(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])))
