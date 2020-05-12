8ca6988cc64d77c4e46a644a59defa4abef5145b
Elijah999 <elijahkr2018@gmail.com>
2020-05-12 13:29:07 -0400

    added 5 different passwords in one run

--- exercise/exercise9.py
+++ exercise/exercise9.py
@@ -1,2 +1,8 @@
 import random
-print(random.choice(["Throwing", "Catching", "Active", "Eating", "Coding", "Sitting", "Watching", "Destroying", "Boarding", "Gaming"])+random.choice(["Chair", "Teammate", "Brownie", "Closet", "Box", "Blanket", "Carpet", "Pillow", "Rug", "Console"])+str(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])))
+num = 0
+while num < 5:
+    password = random.choice(["Throwing", "Catching", "Active", "Eating", "Coding", "Sitting", "Watching", "Destroying", "Boarding", "Gaming"])+random.choice(["Chair", "Teammate", "Brownie", "Closet", "Box", "Blanket", "Carpet", "Pillow", "Muffin", "Console"])+str(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+    print(password)
+    num = num+1
+    if num == 5:
+        break
