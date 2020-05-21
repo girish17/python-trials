4d7df748bceb5660d0f0487b3973e1e88d59c038
gm <25923137+girish17@users.noreply.github.com>
2020-05-21 17:09:10 +0530

    Update exercise11.py

--- exercise/exercise11.py
+++ exercise/exercise11.py
@@ -1,5 +1,13 @@
+'''
+Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number.
+The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+
+Extras:
+    Use binary search.
+'''
+# @author prince
 import math
-#@author prince
+
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 n = int(input("enter a number, and we'll see if it's in the list: "))
 low = 0
