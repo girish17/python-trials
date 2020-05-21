ae470e6cf03438776b26cfea99b54447590cb6d2
gm <25923137+girish17@users.noreply.github.com>
2020-05-21 17:03:53 +0530

    Update exercise8.py

--- exercise/exercise8.py
+++ exercise/exercise8.py
@@ -1,3 +1,8 @@
+'''
+    Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25])
+    and make a new list of only the first and last elements of the given list.
+    For practice, write this code inside a function.
+'''
 def fal():
     a = [5, 10, 15, 20, 25]
     b = ((a[0:5:4]))
